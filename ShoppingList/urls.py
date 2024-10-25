31b4858fd045352152830aa3947fa624c8ad6c09
Matheus Teixeira Lampugnani <matheus.teixeira@volus.com>
2024-10-25 11:35:08 -0300

    Criação de Telas e configurações de login/registro de usuarios

--- ShoppingList/urls.py
+++ ShoppingList/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from ShoppingList.views import index, criarlista
+from ShoppingList.views import index, criarlista, login, registro
 
 urlpatterns = [
     path('', index, name='index'),
-    path('criarlista/', criarlista, name='criarlista')
+    path('criarlista/', criarlista, name='criarlista'),
+    path('login/', login, name='login'),
+    path('registro/', registro, name='registro'),
 ]
